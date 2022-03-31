afd889d37a644fa5e1bbe1136a1947a76862f4ed
Arun <rarnkmrbridgelabz.com>
2022-03-31 20:39:46 +0530

    Added UC-2

--- src/com/bridgelabz/MainClass.java
+++ src/com/bridgelabz/MainClass.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 public class MainClass {
     public static void main(String[] args) {
         HashTable h = new HashTable();
-        String str = "To be or not to be";
+        String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situtations";
         String[] arr = str.split(" ");
         for (String key : arr) {
             h.add(key);
